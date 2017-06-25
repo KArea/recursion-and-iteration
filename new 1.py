85a2fb285b783b0cd1decf900cd84b1d878edfe8
KArea <zkhlly@163.com>
2017-06-25 17:59:54 +0800

    更新代码，用迭代实现斐波那契数列
    加入问题描述，明确数列与时间对应：
    时间：1-2-3-4-5-6-7...
    数列：1-1-2-3-5-8-13...

--- new 1.py
+++ new 1.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-print('迭代实现斐波那契数列求和')
+print('----------迭代实现斐波那契数列----------')
+print('''
+兔子在出生两个月之后就拥有繁殖能力，此后\n
+这对兔子每个月都能产下一对小兔子，假设所\n
+有兔子都不会死去，一年之后可以繁殖多少只\n
+兔子呢？\n''')
 def fab(n):
+    
     n1 = 1
     n2 = 1
     n3 = 1
@@ -9,15 +15,19 @@ def fab(n):
     if n < 1:
         print('输入有误！')
         return -1
+    
+    if n <= 2:
+        return(1)
         
-    while(n-2) > 0:
-        n3 = n2 + n1
+    while n > 2:
+        n3 = n1 + n2
         n1 = n2
         n2 = n3
         n -= 1
         
     return n3
-    
-result = fab(20)
+        
+result = fab(int(input('请输入月数：')))
 if result != -1:
     print('总共有%d对小兔子诞生！'%result)
+        
