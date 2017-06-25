2ba341d179befb58d0d5deab0f4431bbef8975f0
KArea <zkhlly@163.com>
2017-06-25 18:03:10 +0800

    更新代码，用递归实现斐波那契数列求和
    加入问题描述，明确数列与时间对应：
    时间：1-2-3-4-5-6-7...
    数列：1-1-2-3-5-8-13...

--- new 2.py
+++ new 2.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-print('递归实现斐波那契数列求和')
+print('----------递归实现斐波那契数列----------')
+print('''
+兔子在出生两个月之后就拥有繁殖能力，此后\n
+这对兔子每个月都能产下一对小兔子，假设所\n
+有兔子都不会死去，一年之后可以繁殖多少只\n
+兔子呢？\n''')
 def fab(n):
+
     if n < 1:
         print('输入有误')
         return -1
@@ -11,6 +17,6 @@ def fab(n):
     else:
         return fab(n-1) + fab(n-2)
         
-result = fab(20)
+result = fab(int(input('请输入月数：')))
 if result != -1:
     print('总共有%d对小兔崽子诞生！'%result)
